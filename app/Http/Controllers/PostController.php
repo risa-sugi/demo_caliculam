8979c5983312d30c2ba6db1f75e9483df41d2fe2
risa <lisa.elza01@gmail.com>
2021-11-18 19:25:53 +0900

    修正8-5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,12 +7,13 @@ use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
-    public function show(Post $post)
+    public function index(Post $post)
     {
+        return view('posts/index')->with(['posts' => $post->getPaginate()]);
     }
     public function show(Post $post)
     {
-        return view('posts/show')->with(['posts' => $post]);
+        return view('posts/show')->with(['post' => $post]);
     }
     public function create()
     {
@@ -22,7 +23,18 @@ class PostController extends Controller
     {
         $input = $request['post'];
         $post->create($input);
+        
         return redirect('/posts/' . $post->id);
     }
-    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
 }
